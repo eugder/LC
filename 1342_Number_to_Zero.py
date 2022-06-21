26d273e6f00a013045778adc289204aa2eb4d477
eugder <eugder@gmail.com>
2022-06-21 12:55:47 +0300

    Problem 1342 done

---
+++ 1342_Number_to_Zero.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+        while num > 0:
+            if num % 2:
+                num -= 1
+            else:
+                num /= 2
+            steps += 1
+
+        return steps
+
+solution = Solution()
+print(solution.numberOfSteps(123))
