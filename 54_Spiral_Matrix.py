09c19cd29fd93eacb6b7204ee90da191e58545e2
eugder <eugder@gmail.com>
2022-08-30 18:57:16 +0300

    Problem 54 v2 done

--- 54_Spiral_Matrix.py
+++ 54_Spiral_Matrix.py
@@ -1,35 +1,49 @@
 class Solution:
     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
-        i, j = 0, 0
-        width = len(matrix[0])
-        height = len(matrix)
+        # # v1
+        # i, j = 0, 0
+        # width = len(matrix[0])
+        # height = len(matrix)
+        # result = []
+        # starts = (min(width, height) - 1) // 2 + 1
+        # if starts == 0: starts = 1
+        #
+        # for i in range(starts):
+        #     j = i
+        #     result += matrix[j][i:(width-i)]
+        #     for p in range(j+1, height-j):
+        #         result.append(matrix[p][width-i-1])
+        #
+        #     if width*height == len(result):
+        #         break
+        #
+        #     result += matrix[height-j-1][(width-i-2):i:-1]
+        #     for p in range(height-j-1, j, -1):
+        #         result.append(matrix[p][i])
+        #
+        # return result
+
+        #v2
+        size = len(matrix) * len(matrix[0])
         result = []
-        # starts = round(min(width, height)/2)
-        starts = (min(width, height) - 1) // 2 + 1
-        print("width", width, "height", height, "starts", starts)
-        if starts == 0: starts = 1
 
-        for i in range(starts):
-            print(i)
-            j = i
-            result += matrix[j][i:(width-i)]
-            for p in range(j+1, height-j):
-                result.append(matrix[p][width-i-1])
+        while len(result) < size:
+            result += matrix.pop(0)
 
-            if width*height == len(result):
-                print("breack")
-                break
+            for i in matrix:
+                result.append(i.pop(-1))
 
-            result += matrix[height-j-1][(width-i-2):i:-1]
-            for p in range(height-j-1, j, -1):
-                result.append(matrix[p][i])
+            if len(result) == size: break
+            result += matrix.pop(-1)[::-1]
 
-        print(matrix)
+            for i in reversed(matrix):
+                result.append(i.pop(0))
+
         return result
 
 s = Solution()
 # print(s.spiralOrder([[1,2,3]]))
-# print(s.spiralOrder([[3],[2]]))
+# print(s.spiralOrder([[1],[2]]))
 # print(s.spiralOrder([[1,2],[3,4],[5,6]]))
 # print(s.spiralOrder([[1,2,3,4,5],[6,7,8,9,10]]))
 # print(s.spiralOrder([[1,2,3],[4,5,6]]))
